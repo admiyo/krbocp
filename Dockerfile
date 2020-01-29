05b3c78ae61c011e5e161cd5e6142feab9e3910f
Adam Young <ayoung@redhat.com>
2020-01-29 09:29:18 -0500

    display env vars in HTML

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM centos:latest
 MAINTAINER Adam Young
 
 RUN yum -y install httpd mod_wsgi
-
+RUN sed -i.bak 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf
 COPY index.html /var/www/html/
 COPY hello.py   /var/www/cgi-bin/myapp.wsgi
 RUN chmod a+rx  /var/www/cgi-bin/myapp.wsgi
@@ -11,4 +11,4 @@ COPY hello.conf /etc/httpd/conf.d/
 
 CMD /usr/sbin/httpd -D FOREGROUND
 
-EXPOSE 80
+EXPOSE 8080
--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
+from os import environ
+
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello World!'
+    output = b'<html><head><title>Environemnt Variables</title></head><body><dl>'
 
-    response_headers = [('Content-type', 'text/plain'),
+
+    keys = environ.keys()
+    for key in keys:
+        line = '<dt>%s</dt><dd> %s</dd>\n' % (key, repr(environ[key]))
+        output += line.encode('utf-8')
+    output += b'</dl></body></html>'
+
+    response_headers = [('Content-type', 'text/html'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
 
